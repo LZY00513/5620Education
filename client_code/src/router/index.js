bcdb80ac52d18e1267df0ccdc81173d3645979ca
WENXIN <wshi0830@uni.sydney.edu.au>
2024-10-27 18:59:47 +1100

    Changes1-swx

--- client_code/src/router/index.js
+++ client_code/src/router/index.js
@@ -232,7 +232,7 @@ const routes = [{
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),cc
   routes
 })
 
